88925d475e561f18772fb3ea0de4202079337ede
Balaji Dubey <balajidubey214@gmail.com>
2016-03-13 15:33:49 +0530

    Remove IDE generated java doc

--- chap2_fundamentals_of_analysis_of_algorithms_efficiency/MatrixMultiplication.java
+++ chap2_fundamentals_of_analysis_of_algorithms_efficiency/MatrixMultiplication.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package chap2_fundamentals_of_analysis_of_algorithms_efficiency;
 
 /**
@@ -9,5 +5,5 @@ package chap2_fundamentals_of_analysis_of_algorithms_efficiency;
  * @author Balaji Dubey
  */
 public class MatrixMultiplication {
-    
+//Todo: complete this    
 }
--- chap3_brute_force/SelectionSort.java
+++ chap3_brute_force/SelectionSort.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package chap3_brute_force;
 
 import java.util.Scanner;
